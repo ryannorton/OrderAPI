f2e33fa844362f6eb63764fad34ad30450dddabe
ryannorton <nort.ryan@gmail.com>
2014-11-27 12:11:54 -0600

    updated models

--- app/models.py
+++ app/models.py
@@ -1,38 +1,46 @@
 from django.db import models
 
 class Customer(models.Model):
-	first_name = models.CharField()
-	last_name = models.CharField()
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
 	email = models.EmailField(unique=True)
-	phone = models.CharField()
+	phone = models.CharField(max_length=12)
 
 class Item(models.Model):
-	title = models.CharField()
+	title = models.CharField(max_length=1000)
 	description = models.TextField()
-	unit_price = models.DecimalField(decimal_places=2)
-	SKU = models.CharField()
+	unit_price = models.DecimalField(max_digits=9, decimal_places=2)
+	SKU = models.CharField(max_length=1000)
 
 class Payment(models.Model):
-	credit_card_company = models.CharField()
-	credit_card_number = models.CharField()
-	credit_card_ccv = models.CharField()
+	credit_card_company = models.CharField(max_length=50)
+	credit_card_number = models.CharField(max_length=100)
+	credit_card_ccv = models.CharField(max_length=5)
 
-class Address(models.Model):
-	address1 = models.CharField()
-	address2 = models.CharField()
-	city = models.CharField()
-	state = models.CharField()
-	zipcode = models.CharField()
-	country = models.CharField(default='USA')
+class BillingAddress(models.Model):
+	address1 = models.CharField(max_length=1000)
+	address2 = models.CharField(max_length=1000)
+	city = models.CharField(max_length=100)
+	state = models.CharField(max_length=2)
+	zipcode = models.CharField(max_length=15)
+	country = models.CharField(max_length=100, default='USA')
+
+class ShippingAddress(models.Model):
+	address1 = models.CharField(max_length=1000)
+	address2 = models.CharField(max_length=1000)
+	city = models.CharField(max_length=100)
+	state = models.CharField(max_length=2)
+	zipcode = models.CharField(max_length=15)
+	country = models.CharField(max_length=100, default='USA')
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer)
-	shipping_address = models.ForeignKey(Address)
-	billing_address = models.ForeignKey(Address)
+	shipping_address = models.ForeignKey(ShippingAddress)
+	billing_address = models.ForeignKey(BillingAddress)
 	payment_details = models.ForeignKey(Payment)
-	total_price = models.DecimalField(decimal_places=2)
-	subtotal_price = models.DecimalField(decimal_places=2)
-	total_tax = models.DecimalField(decimal_places=2)
+	total_price = models.DecimalField(max_digits=9 ,decimal_places=2)
+	subtotal_price = models.DecimalField(max_digits=9, decimal_places=2)
+	total_tax = models.DecimalField(max_digits=9, decimal_places=2)
 	order_date = models.DateTimeField(auto_now_add=True)
 
 class OrderItem(models.Model):
