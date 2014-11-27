6e7d0efbb363fe52a49e7d899a60b648452e9ffc
ryannorton <nort.ryan@gmail.com>
2014-11-27 14:15:36 -0600

    added order list and detailed view endpoints

--- orderapi/views.py
+++ orderapi/views.py
@@ -1,50 +1,55 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from orderapi.models import Order
-import json
 
-orders = []
-
-testOrder = {
-	'id' : '1',
-	'customerID' : '3471328947',
-	'items' : [
-		{
-			'quantity' : 4,
-			'price' : 10.00,
-			'SKU' : 'sadofj',
-		},
-	],
-	'createdTime' : 'testDate',
-	'shippingLocation' : {
-		'street' : 'testStreet',
-		'zipcode' : '93843',
-	},
-	'billingLocation' : {
-		'street' : 'testStreet',
-		'zipcode' : '93843',
-	},
-	'price' : {
-		'totalPrice' : '4.0',
-		'tax' : '2.0',
-		'shipping' : '1.0',
-		'subTotal' : '3.0',
-	},
-}
-
-orders.append(testOrder)
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from orderapi.serializers import OrderSerializer
 
 def index(request):
 	return HttpResponse("it works")
 
-def orderSummary(request):
-	return HttpResponse("orders page")
+@api_view(['GET', 'POST'])
+def orderList(request):
+	"""
+	List all orders, or create a new order.
+	"""
+	if request.method == 'GET':
+		orders = Order.objects.all()
+		serializer = OrderSerializer(orders, many=True)
+		return Response(serializer.data)
 
+	elif request.method == 'POST':
+		serializer = OrderSerializer(data=request.DATA)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def orderDetailed(request, pk):
-	order = Order.objects.get(id=pk)
-	return HttpResponse(json.dumps(order), content_type="application/json")
+	"""
+	Retrieve, update, or delete an order instance 
+	"""
+	try:
+		order = Order.objects.get(id=pk)
+	except:
+		return Response(status=status.HTTP_404_NOT_FOUND)
 
-	# for order in orders:
-	# 	if order['id'] == pk:
-	# 		return HttpResponse(json.dumps(order), content_type="application/json")
-	return HttpResponse("nope")
+	if request.method == 'GET':
+		serializer = OrderSerializer(order)
+		return Response(serializer.data)
+
+	elif request.method == 'PUT':
+		serializer = OrderSerializer(order, data=request.DATA)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
