45155545f9d24c85ebc8ba5858bc9f614bfae9ba
ryannorton <nort.ryan@gmail.com>
2014-11-27 20:18:25 -0600

    added customer endpoints

--- Ordoro/urls.py
+++ Ordoro/urls.py
@@ -6,6 +6,8 @@ urlpatterns = patterns('',
 	url(r'^$', views.index, name="index"),
 	url(r'^api/orders/$', views.orderList, name='orders'),
     url(r'^api/orders/(?P<pk>.+)/$', views.orderDetailed, name='orderDetailed'),
+    url(r'^api/customers/$', views.customerList, name='customers'),
+    url(r'^api/customers/(?P<pk>.+)/$', views.customerDetailed, name='customerDetailed')
 )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- orderapi/views.py
+++ orderapi/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from orderapi.models import Order
-
+from orderapi.models import Order, Customer
+from orderapi.serializers import OrderSerializer, CustomerSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from orderapi.serializers import OrderSerializer
 
 def index(request):
 	return HttpResponse("it works")
@@ -52,4 +51,44 @@ def orderDetailed(request, pk, format=None):
 		order.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
 
+@api_view(['GET', 'POST'])
+def customerList(request, format=None):
+	"""
+	List all customers, or create a new customer.
+	"""
+	if request.method == 'GET':
+		customers = Customer.objects.all()
+		serializer = CustomerSerializer(customers, many=True)
+		return Response(serializer.data)
 
+	elif request.method == 'POST':
+		serializer = CustomerSerializer(data=request.DATA)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def customerDetailed(request, pk, format=None):
+	"""
+	Retrieve, update, or delete a customer instance 
+	"""
+	try:
+		customer = Customer.objects.get(id=pk)
+	except:
+		return Response(status=status.HTTP_404_NOT_FOUND)
+
+	if request.method == 'GET':
+		serializer = CustomerSerializer(customer)
+		return Response(serializer.data)
+
+	elif request.method == 'PUT':
+		serializer = CustomerSerializer(customer, data=request.DATA)
+		if serializer.is_valid():
+			serializer.save()
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+	elif request.method == 'DELETE':
+		customer.delete()
+		return Response(status=status.HTTP_204_NO_CONTENT)
